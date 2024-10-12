22cb41075888135437ce292e72936746f6283601
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-12 15:23:22 +0330

    [WIP]

--- core/todo/views.py
+++ core/todo/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from .forms import TaskUpdateForm
+from .models import Task
+
 
 # Create your views here.
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'todo/task_list.html'
+    context_object_name = "tasks"
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ["title"]
+    success_url = reverse_lazy("task_list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreateView, self).form_valid(form)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    success_url = reverse_lazy("task_list")
+    form_class = TaskUpdateForm
+    template_name = "todo/update_task.html"
+
+
+class TaskCompleteView(LoginRequiredMixin, View):
+    model = Task
+    success_url = reverse_lazy("task_list")
+
+    def get(self, request, *args, **kwargs):
+        object = Task.objects.get(id=kwargs.get("pk"))
+        object.complete = True
+        object.save()
+        return redirect(self.success_url)
+
+
+class DeleteTaskView(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = "task"
+    success_url = reverse_lazy("task_list")
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
